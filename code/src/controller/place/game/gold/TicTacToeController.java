94a210377747402d9bb2c3c2a4dc4a5909854cc0
Goubeau Samuel <sams86240@gmail.com>
2021-04-28 12:18:41 +0200

    Add element view for TicTacToe

--- code/src/controller/place/game/gold/TicTacToeController.java
+++ code/src/controller/place/game/gold/TicTacToeController.java
@@ -20,9 +20,68 @@ public class TicTacToeController implements Initializable {
     private Player player;
     private Scene scene;
 
+
+    // ================================
+    //  ELEMENTS IN VIEW
+    // ================================
+
     @FXML
     private ImageView goldHubIcon;
 
+    @FXML
+    private ImageView crossCenter;
+
+    @FXML
+    private ImageView crossLeft;
+
+    @FXML
+    private ImageView crossRight;
+
+    @FXML
+    private ImageView crossTop;
+
+    @FXML
+    private ImageView crossTopLeft;
+
+    @FXML
+    private ImageView crossTopRight;
+
+    @FXML
+    private ImageView crossBot;
+
+    @FXML
+    private ImageView crossBotLeft;
+
+    @FXML
+    private ImageView crossBotRight;
+
+    @FXML
+    private ImageView circleCenter;
+
+
+    // ================================
+    //  SETTERS
+    // ================================
+
+    public void setGameController(GameController gameController) {
+        this.gameController = gameController;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    public void setScene(Scene scene) {
+        this.scene = scene;
+    }
+
+
+    // ================================
+    //  EVENTS ON ELEMENTS
+    // ================================
+
+
+    //-- Icon goGold
     @FXML
     void iconMouseEntered(MouseEvent mouseEvent) {
         UtilsController.rescaleNode(this.scene, (ImageView) mouseEvent.getTarget(), 1.2);
@@ -39,20 +98,34 @@ public class TicTacToeController implements Initializable {
         this.gameController.changePlace();
     }
 
+    //-- Icon Cross (and maybe circle)
+    @FXML
+    void iconCrossMouseEntered(MouseEvent mouseEvent) {
+        ImageView crossTarget = (ImageView) mouseEvent.getTarget();
+        crossTarget.setOpacity(0.25);
+    }
+
+    @FXML
+    void iconCrossMouseExited(MouseEvent mouseEvent) {
+        ImageView crossTarget = (ImageView) mouseEvent.getTarget();
+        crossTarget.setOpacity(0);
+    }
+
+    @FXML
+    void iconCrossMouseClicked(MouseEvent mouseEvent) {
+        ImageView crossTarget = (ImageView) mouseEvent.getTarget();
+        crossTarget.setOpacity(1);
+    }
+
+
+    // ================================
+    //  ACTIONS ON GAME AND ELEMENTS
+    // ================================
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         Tooltip.install(this.goldHubIcon, new Tooltip("Go to gold hub"));
     }
 
-    public void setGameController(GameController gameController) {
-        this.gameController = gameController;
-    }
 
-    public void setPlayer(Player player) {
-        this.player = player;
-    }
-
-    public void setScene(Scene scene) {
-        this.scene = scene;
-    }
 }
